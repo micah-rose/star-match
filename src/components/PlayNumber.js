e8c51c550cca26614a8547532189a401b35fd24e
Micah Lund <micah.r.lund@gmail.com>
2020-09-01 16:09:08 -0600

    Set up an onclick property in the PlayNumber component.

--- src/components/PlayNumber.js
+++ src/components/PlayNumber.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Number = props => (
-    <button key={props.number} className="number">{props.number}</button> 
+    <button 
+    key={props.number} 
+    className="number"
+    onClick={() => console.log('Num', props.number)}>
+        {props.number}
+    </button> 
 )
 
 export default Number;
