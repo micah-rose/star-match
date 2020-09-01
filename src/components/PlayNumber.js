726fa99240cfff9b967d2d9fa583b66b3d7e13b9
Micah Lund <micah.r.lund@gmail.com>
2020-09-01 17:31:29 -0600

    Ran into another problem where I need to access constants from other components. Will need to rethink how this is organized so I can have a working game.

--- src/components/PlayNumber.js
+++ src/components/PlayNumber.js
@@ -4,6 +4,9 @@ const PlayNumber = props => (
     <button 
     key={props.number} 
     className="number"
+    style={{
+        backgroundColor: colors[props.status]
+    }}
     onClick={() => console.log('Num', props.number)}>
         {props.number}
     </button> 
