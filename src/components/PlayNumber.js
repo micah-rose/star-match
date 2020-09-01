bb8b77c54865bd9615871be276fc8af12b6d02dc
Micah Lund <micah.r.lund@gmail.com>
2020-09-01 16:10:29 -0600

    Forgot some places where Number needed to be PlayNumber.

--- src/components/PlayNumber.js
+++ src/components/PlayNumber.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Number = props => (
+const PlayNumber = props => (
     <button 
     key={props.number} 
     className="number"
@@ -9,4 +9,4 @@ const Number = props => (
     </button> 
 )
 
-export default Number;
+export default PlayNumber;
