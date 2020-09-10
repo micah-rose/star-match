26ca1429973da1274f73401b44adbeb56624d6c1
Micah Lund <micah.r.lund@gmail.com>
2020-09-10 16:50:15 -0600

    Made some modifications to simplify the components and removed unnecessary code. Also determined that the issue with my play again button not working is happening in the Game component.

--- src/components/StarMatch.js
+++ src/components/StarMatch.js
@@ -3,11 +3,7 @@ import Game from './Game';
 
 const StarMatch = () => {
     const [gameId, setGameId] = useState(1);
-    return <Game key={gameId} newGame={console.log('Button clicked')}/>;
+    return <Game key={gameId} newGame={() => setGameId(gameId + 1)}/>;
 }
-
-//() => setGameId(gameId + 1)
-
-//https://jscomplete.com/playground/rgs3.9 >> Finished code for this project, will need it to figure out what I am missing in my code
 
 export default StarMatch;
