df3aefece1c4b5bcf3a46b8fba3b023a36748a4a
Micah Lund <micah.r.lund@gmail.com>
2020-11-27 16:08:02 -0700

    Updates

---
+++ precache-manifest.184c84c17af1f65c4ba468329fea05cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5f2129418158a77db38cc6c4fb437e",
+    "url": "/star-match/index.html"
+  },
+  {
+    "revision": "4274c3c213c46e1d370f",
+    "url": "/star-match/static/css/main.14113484.chunk.css"
+  },
+  {
+    "revision": "27849bb0ecfe930cd079",
+    "url": "/star-match/static/js/2.a747edd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-match/static/js/2.a747edd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4274c3c213c46e1d370f",
+    "url": "/star-match/static/js/main.760b02da.chunk.js"
+  },
+  {
+    "revision": "8ff643d5cb965ebfbbff",
+    "url": "/star-match/static/js/runtime-main.d0ddbd5c.js"
+  }
+]);
